48d31fa27ab947f9121b8268da8f6fe1d6fed9a2
Artem <hooch19@mail.ru>
2025-01-04 21:27:17 +0300

    Updated files in sqlite_to_postgres

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -1,22 +1,28 @@
 import sqlite3
-
 import psycopg
 from psycopg import ClientCursor, connection as _connection
 from psycopg.rows import dict_row
+from postgres_saver import PostgresSaver
+from sqlite_loader import SQLiteLoader
+from test_transfer import TestTransfer
 
-
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection, table_name: str):
     """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
+    postgres_saver = PostgresSaver(pg_conn, table_name)
+    sqlite_loader = SQLiteLoader(connection, table_name)
 
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+    data = sqlite_loader.load_movies()
+
+    postgres_saver.save_all_data(data)
 
 
 if __name__ == '__main__':
     dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg.connect(
-        **dsl, row_factory=dict_row, cursor_factory=ClientCursor
+    with sqlite3.connect('sqlite.db') as sqlite_conn, psycopg.connect(
+            **dsl, row_factory=dict_row, cursor_factory=ClientCursor
     ) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+        for table_name in ['film_work', 'genre', 'genre_film_work', 'person', 'person_film_work']:
+            load_from_sqlite(sqlite_conn, pg_conn, table_name)
+        for table_name in ['film_work', 'genre', 'genre_film_work', 'person', 'person_film_work']:
+            TestTransfer(sqlite_conn, pg_conn, table_name).test_transfer()
+            print(f'Table {table_name} was successfully transferred to PostgreSQL.')
